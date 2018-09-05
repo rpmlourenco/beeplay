46acdcd5edc37c6a57a2205dc7b463cb3905623a
rpmlourenco <rpmlourenco@gmail.com>
2018-09-05 18:59:25 +0200

    migration ok!

--- pre-post-commands/post-build-winamp.bat
+++ pre-post-commands/post-build-winamp.bat
@@ -1,3 +1,4 @@
-rem IF EXIST "%1..\Music Players\MusicBee\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MusicBee\Plugins\"
+rem echo "copy %2 to %1..Music Players\MusicBee\Plugins\"
+IF EXIST "%1..\Music Players\MusicBee\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MusicBee\Plugins\"
 rem IF EXIST "%1..\Music Players\Winamp\Plugins\" COPY /B /Y "%2" "%1..\Music Players\Winamp\Plugins\"
 rem IF EXIST "%1..\Music Players\MediaMonkey\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MediaMonkey\Plugins\"
