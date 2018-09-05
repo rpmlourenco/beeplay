2a8434b57a5772b65de33e6ad7149f3dbc3306ed
Rui Lourenço <rpmlourenco@gmail.com>
2018-09-05 07:54:22 +0100

    debug working directory

--- pre-post-commands/post-build-winamp.bat
+++ pre-post-commands/post-build-winamp.bat
@@ -1,3 +1,3 @@
-IF EXIST "%1..\Music Players\MusicBee\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MusicBee\Plugins\"
-IF EXIST "%1..\Music Players\Winamp\Plugins\" COPY /B /Y "%2" "%1..\Music Players\Winamp\Plugins\"
-IF EXIST "%1..\Music Players\MediaMonkey\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MediaMonkey\Plugins\"
+rem IF EXIST "%1..\Music Players\MusicBee\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MusicBee\Plugins\"
+rem IF EXIST "%1..\Music Players\Winamp\Plugins\" COPY /B /Y "%2" "%1..\Music Players\Winamp\Plugins\"
+rem IF EXIST "%1..\Music Players\MediaMonkey\Plugins\" COPY /B /Y "%2" "%1..\Music Players\MediaMonkey\Plugins\"
--- pre-post-commands/pre-build-winamp.bat
+++ pre-post-commands/pre-build-winamp.bat
@@ -1,3 +1,3 @@
-IF EXIST "%1..\Music Players\MusicBee\Plugins\" ERASE /F /Q "%1..\Music Players\MusicBee\Plugins\%2"
-IF EXIST "%1..\Music Players\Winamp\Plugins\" ERASE /F /Q "%1..\Music Players\Winamp\Plugins\%2"
-IF EXIST "%1..\Music Players\MediaMonkey\Plugins\" ERASE /F /Q "%1..\Music Players\MediaMonkey\Plugins\%2"
+rem IF EXIST "%1..\Music Players\MusicBee\Plugins\" ERASE /F /Q "%1..\Music Players\MusicBee\Plugins\%2"
+rem IF EXIST "%1..\Music Players\Winamp\Plugins\" ERASE /F /Q "%1..\Music Players\Winamp\Plugins\%2"
+rem IF EXIST "%1..\Music Players\MediaMonkey\Plugins\" ERASE /F /Q "%1..\Music Players\MediaMonkey\Plugins\%2"
